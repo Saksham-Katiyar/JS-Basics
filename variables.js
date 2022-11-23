833962f8549cb36aff2a04bab46e05cbaf983885
Saksham Katiyar <20d100022@iitb.ac.in>
2022-11-23 06:57:24 +0000

    Create cowsay.js

---
+++ variables.js
@@ -0,0 +1,18 @@
+var a = 10;
+const b = 20;
+let c = 30;
+
+const Aditya = "paad"; //
+a = 40;
+c = 50;
+// console.log(a);
+
+if (3 > 2 > 1)
+{
+    console.log("equal")
+}
+else
+{
+    console.log("not equal")
+}
+
